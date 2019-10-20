ec333892a64692991460fd31fc7c830056e841c0
AsemSkakova <asemaskakova@gmail.com>
2019-10-20 13:47:09 +0600

    myv

--- raid1a.go
+++ raid1a.go
@@ -4,17 +4,19 @@ import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
 	if x > 0 && y > 0 {
-		for i := 1; i <= x; i++ {
-			if i == 1 || i == x {
+		for i := 0; i < x; i++ {
+			if i == 0 || i == x-1 {
 				z01.PrintRune('o')
 			} else {
 				z01.PrintRune('-')
 			}
 		}
+
 		z01.PrintRune(10)
-		for i := 2; i <= y-1; i++ {
-			for j := 1; j <= x; j++ {
-				if j == 1 || j == x {
+
+		for i := 0; i < y-2; i++ {
+			for j := 0; j < x; j++ {
+				if j == 0 || j == x-1 {
 					z01.PrintRune('|')
 				} else {
 					z01.PrintRune(' ')
@@ -22,15 +24,16 @@ func Raid1a(x, y int) {
 			}
 			z01.PrintRune(10)
 		}
+
 		if y != 1 {
-			for i := 1; i <= x; i++ {
-				if i == 1 || i == x {
+			for i := 0; i < x; i++ {
+				if i == 0 || i == x-1 {
 					z01.PrintRune('o')
 				} else {
 					z01.PrintRune('-')
 				}
 			}
-			z01.PrintRune(10)
 		}
+		z01.PrintRune(10)
 	}
 }
